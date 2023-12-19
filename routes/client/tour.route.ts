f3e50a922b20e0b4cba00e653c01fcafcc2943ba
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-19 20:07:41 +0700

    2.2. Tạo controllers

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,16 +1,9 @@
-import { Request, Response } from "express";
 import { Router } from "express";
-import Tour from "../../models/tour.model";
+
+import * as controller from "../../controllers/client/tour.controller";
+
 const router: Router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    raw: true
-  });
-
-  res.render("client/pages/tours/index", {
-    tours: tours
-  });
-});
+router.get("/", controller.index);
 
 export const tourRoutes: Router = router;
