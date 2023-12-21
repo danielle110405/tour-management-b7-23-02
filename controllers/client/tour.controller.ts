1cd4058013eb8d513a34c3a2821a42ef817bea3b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-21 19:29:36 +0700

    1.2. Lấy data in ra giao diện

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-// import Tour from "../../models/tour.model";
+import Tour from "../../models/tour.model";
 import sequelize from "../../config/database";
 import { QueryTypes } from "sequelize";
 
@@ -65,9 +65,25 @@ export const index = async (req: Request, res: Response) => {
 export const detail = async (req: Request, res: Response) => {
   const slugTour = req.params.slugTour;
 
-  console.log(slugTour);
+  const tourDetail = await Tour.findOne({
+    where: {
+      slug: slugTour,
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+
+  if(tourDetail["images"]) {
+    tourDetail["images"] = JSON.parse(tourDetail["images"]);
+  }
+  
+  tourDetail["price_special"] = tourDetail["price"] * (1 - tourDetail["discount"]/100);
+
+  console.log(tourDetail);
   
   res.render("client/pages/tours/detail", {
     pageTitle: "Chi tiết tour",
+    tourDetail: tourDetail
   });
 };
