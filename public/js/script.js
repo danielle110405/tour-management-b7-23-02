ff3f848585306aef884a0c6bb3adef83f44eee85
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-21 20:14:21 +0700

    2.1. Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,41 @@ var imagesMain = new Swiper(".imagesMain", {
   },
 });
 // End Slider Tour Detail
+
+// Carts
+
+// Nếu chưa có giỏ hàng trong localStorage thì tạo giỏ hàng mới cho người dùng
+const cart = localStorage.getItem("cart");
+if(!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if(quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+
+      if(indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity 
+        });
+      } else {
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Carts
