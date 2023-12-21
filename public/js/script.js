ebda56e134d0eea7120d0c45e4fa3696ff6ae99c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-21 20:40:24 +0700

    2.3. Làm nút Mini Cart trên Header

--- public/js/script.js
+++ public/js/script.js
@@ -41,6 +41,17 @@ if(!cart) {
   localStorage.setItem("cart", JSON.stringify([]));
 }
 
+// Hiển thị thêm số lượng sản phẩm vào mini-cart
+const showMiniCart = () => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if(miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+    miniCart.innerHTML = totalQuantity;
+  }
+}
+showMiniCart();
+
 // Thêm tour vào cart
 const formAddToCart = document.querySelector("[form-add-to-cart]");
 if(formAddToCart) {
@@ -67,6 +78,8 @@ if(formAddToCart) {
       localStorage.setItem("cart", JSON.stringify(cart));
 
       alertAddCartSuccess();
+
+      showMiniCart();
     }
   });
 }
