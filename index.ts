ef894f643b26658b408faa453cf4ae86bbbdf1d5
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-23 19:38:37 +0700

    3.2. Lấy data từ database trả ra giao diện

--- index.ts
+++ index.ts
@@ -8,6 +8,9 @@ dotenv.config();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.set("views", "./views");
