02d247b7054f65f66f0c041c6f9dfc25f33f8186
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-26 19:24:08 +0700

    3.2. Lấy data hiển thị ra giao diện

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -61,7 +61,47 @@ export const order = async (req: Request, res: Response) => {
 
 // [GET] /order/success
 export const success = async (req: Request, res: Response) => {
+  const orderCode = req.query.orderCode;
+
+  const order = await Order.findOne({
+    where: {
+      code: orderCode,
+      deleted: false
+    },
+    raw: true
+  });
+
+  const ordersItem = await OrderItem.findAll({
+    where: {
+      orderId: order["id"]
+    },
+    raw: true
+  });
+
+  for (const item of ordersItem) {
+    item["price_special"] = item["price"] * (1 - item["discount"]/100);
+    item["total"] = item["price_special"] * item["quantity"];
+
+    const infoTour = await Tour.findOne({
+      where: {
+        id: item["tourId"]
+      },
+      raw: true
+    });
+
+    item["title"] = infoTour["title"];
+    item["slug"] = infoTour["slug"];
+    item["image"] = JSON.parse(infoTour["images"])[0];
+  }
+
+  order["total_price"] = ordersItem.reduce((sum, item) => sum + item["total"], 0);
+
+  console.log(order);
+  console.log(ordersItem);
+
   res.render("client/pages/order/success", {
-    pageTitle: "Đặt hàng thành công"
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+    ordersItem: ordersItem
   });
 };
