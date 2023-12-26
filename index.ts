8cf1c021b58c998095276fca79009657c2aa5409
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-26 21:03:17 +0700

    2.3. Bổ sung thông tin tour, lịch trình tour

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import moment from "moment";
 import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
 import { systemConfig } from "./config/system";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,13 @@ app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
 
 // App Local Variables
 app.locals.moment = moment;
